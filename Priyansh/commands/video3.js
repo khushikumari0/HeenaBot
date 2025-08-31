5d39b7489a30912f6cd6a2d763b0aeaff9c38c2e
Sksameersk86 <146408638+Sksameersk86@users.noreply.github.com>
2025-08-31 17:48:56 +0530

    Update video3.js
    Signed-off-by: Sksameersk86 <146408638+Sksameersk86@users.noreply.github.com>

--- Priyansh/commands/video3.js
+++ Priyansh/commands/video3.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 module.exports = {
   config: {
-    name: "xvideo",
+    name: "xxx",
     aliases: [],
     description: "Download and send random videos from xvideos API",
     usage: "{prefix}randomxvideos [page]",
